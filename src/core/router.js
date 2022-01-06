e4837fb6ad0e23dac57fd0104c9139a9ca694b45
aedenmurray <aeden@aedenmurray.io>
2022-01-05 17:45:45 -0700

    Update

--- src/core/router.js
+++ src/core/router.js
@@ -1,37 +1,52 @@
 import { Router } from 'express';
 import validateRequest from '../middleware/validateRequest';
 
+const requestStart = Symbol('Determine request execution time.');
+
+const startRequestTimer = (response) => {
+    if (!response.locals[requestStart])
+        response.locals[requestStart] = new Date();
+};
+
+const setupMiddleware = (fn, logFn) => (request, response, next) => {
+    startRequestTimer(response);
+    Promise.resolve(fn(request, response, next, logFn)).then(next).catch(next);
+};
+
+const setupHandler = (fn, logFn) => (request, response, next) => {
+    startRequestTimer(response);
+
+    Promise.resolve(fn(request, response, next, logFn))
+        .then((responseData = {}) => {
+            const executionTime = new Date() - response.locals[requestStart];
+            logFn(`Request Finished. (${executionTime}ms)`);
+
+            if (response.headersSent) return next();
+            return response.json(responseData);
+        })
+        .catch(next);
+};
+
 export default (wrappedFunction) => {
-    const expressRouter = Router();
+    const router = Router();
 
     const route = (config) => {
-        const { path, method, schema, handler } = config;
+        const { path, method, schema, handler, logPrefix } = config;
         const middleware = config.middleware ?? [];
-        const httpMethod = method.toLowerCase();
 
         if (!path) throw new Error('Missing "path" from route config!');
         if (!method) throw new Error('Missing "method" from route config!');
         if (!handler) throw new Error('Missing "handler" from route config!');
         if (schema) middleware.unshift(validateRequest(schema));
 
-        middleware.forEach((fn) => {
-            expressRouter[httpMethod](path, (request, response, next) => {
-                Promise.resolve(fn(request, response, next))
-                    .then(next)
-                    .catch(next);
-            });
-        });
+        const log = (message) =>
+            // eslint-disable-next-line no-console
+            console.log(`${logPrefix || 'Conductive'}: ${message}`);
 
-        expressRouter[httpMethod](path, (request, response, next) => {
-            Promise.resolve(handler(request, response, next))
-                .then((responseJSON = {}) => {
-                    if (response.headersSent) return next();
-                    return response.json(responseJSON);
-                })
-                .catch(next);
-        });
+        middleware.map((fn) => router[method](path, setupMiddleware(fn, log)));
+        router[method](path, setupHandler(handler, log));
     };
 
     wrappedFunction(route);
-    return expressRouter;
+    return router;
 };
