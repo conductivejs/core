e272fe3f1155b400ffa8cc698c648bdd8c34f5aa
aedenmurray <aeden@aedenmurray.io>
2022-01-08 15:26:21 -0700

    update

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,25 @@
-import { babel } from '@rollup/plugin-babel';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
 
 export default {
     input: 'src/core.js',
 
-    output: {
-        dir: 'bin/',
-        format: 'cjs',
-        entryFileNames: '[name].js',
-        chunkFileNames: '[name].js',
-    },
+    output: [
+        {
+            dir: 'bin',
+            format: 'cjs',
+            entryFileNames: '[name].cjs',
+            chunkFileNames: '[name].cjs',
+        },
+        {
+            dir: 'bin',
+            format: 'esm',
+            entryFileNames: '[name].mjs',
+            chunkFileNames: '[name].mjs',
+        },
+    ],
 
-    manualChunks: {
-        errors: ['src/errors/index.js'],
-        middleware: ['src/middleware/index.js'],
-    },
-
-    plugins: [babel({ babelHelpers: 'runtime' }), nodeResolve(), terser()],
+    plugins: [nodeResolve(), terser()],
 
     external: [
         'ajv',
