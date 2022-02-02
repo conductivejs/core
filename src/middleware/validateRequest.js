c28ae718f02b2bd26e0ed12c7d0ed7e519456341
aedenmurray <aeden@aedenmurray.io>
2022-02-02 14:48:04 -0700

    testing

--- src/middleware/validateRequest.js
+++ src/middleware/validateRequest.js
@@ -4,17 +4,18 @@ import ajvFormats from 'ajv-formats';
 import GenericError from '../errors/GenericError';
 
 export default (schema) => (request) => {
-    // const ajv = new AJV({ allErrors: true });
-    // ajvErrors(ajv);
-    // ajvFormats(ajv);
-    // const requestProperties = Object.keys(schema);
-    // requestProperties.forEach((property) => {
-    //     const propertyData = request[property];
-    //     const propertySchema = schema[property];
-    //     if (!ajv.validate(propertySchema, propertyData)) {
-    //         throw ajv.errors.map(
-    //             ({ message }) => new GenericError(400, message)
-    //         );
-    //     }
-    // });
+    const ajv = new AJV({ allErrors: true });
+    ajvErrors(ajv);
+    ajvFormats(ajv);
+
+    const requestProperties = Object.keys(schema);
+    requestProperties.forEach((property) => {
+        const propertyData = request[property];
+        const propertySchema = schema[property];
+        if (!ajv.validate(propertySchema, propertyData)) {
+            throw ajv.errors.map(
+                ({ message }) => new GenericError(400, message)
+            );
+        }
+    });
 };
