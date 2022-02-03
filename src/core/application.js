9272dff852ad6945402b6b64fa7c70d6bfb18e4d
aedenmurray <aeden@aedenmurray.io>
2022-02-03 12:21:16 -0700

    add logging cors

--- src/core/application.js
+++ src/core/application.js
@@ -1,16 +1,39 @@
 import cors from 'cors';
 import helmet from 'helmet';
 import express from 'express';
+import logFn from './logFn';
 
 export const options = Symbol('Configure Conductive options.');
 
 export default (config = {}) => {
     const app = express();
+
+    app[options] = {
+        logging: config.logging ?? true,
+    };
+
+    if (app[options].logging) {
+        const log = logFn('Conductive');
+
+        const logCORS = (request, response, next) => {
+            const method =
+                request.method &&
+                request.method.toUpperCase &&
+                request.method.toUpperCase();
+
+            if (method === 'OPTIIONS') {
+                log('Handling CORS request...');
+            }
+
+            next();
+        };
+
+        app.use(logCORS);
+    }
+
     app.use(cors(config.cors));
     app.use(helmet(config.helmet));
     app.use(express.json(config.json));
-
-    app[options] = { logging: config.logging ?? true };
 
     return app;
 };
