9b776343d10d36efb7dc825a75de2a122d4a5b28
aedenmurray <aeden@aedenmurray.io>
2022-02-08 12:21:38 -0700

    Update validateRequest

--- src/middleware/validateRequest.js
+++ src/middleware/validateRequest.js
@@ -3,19 +3,25 @@ import ajvErrors from 'ajv-errors';
 import ajvFormats from 'ajv-formats';
 import GenericError from '../errors/GenericError';
 
-export default (schema) => (request) => {
+export default (schema) => {
     const ajv = new AJV({ allErrors: true });
     ajvErrors(ajv);
     ajvFormats(ajv);
 
     const requestProperties = Object.keys(schema);
-    requestProperties.forEach((property) => {
-        const propertyData = request[property];
-        const propertySchema = schema[property];
-        if (!ajv.validate(propertySchema, propertyData)) {
-            throw ajv.errors.map(
-                ({ message }) => new GenericError(400, message)
-            );
-        }
-    });
+    return (request, response, next, log) => {
+        log('Validating schema...');
+
+        requestProperties.forEach((property) => {
+            const propertyData = request[property];
+            const propertySchema = schema[property];
+            if (!ajv.validate(propertySchema, propertyData)) {
+                throw ajv.errors.map(
+                    ({ message }) => new GenericError(400, message)
+                );
+            }
+        });
+
+        log('Finished validating schema.');
+    };
 };
