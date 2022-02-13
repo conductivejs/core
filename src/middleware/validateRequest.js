dbdc9065eac6c6e52f2c513dbfed91404b2cdb1e
aedenmurray <aeden@aedenmurray.io>
2022-02-13 14:36:13 -0700

    Update

--- src/middleware/validateRequest.js
+++ src/middleware/validateRequest.js
@@ -3,18 +3,31 @@ import ajvErrors from 'ajv-errors';
 import ajvFormats from 'ajv-formats';
 import GenericError from '../errors/GenericError';
 
+const ajv = new AJV({ allErrors: true });
+ajvErrors(ajv);
+ajvFormats(ajv);
+
 export default (schema) => {
+    const compiledValidationFunctions = {};
     const requestProperties = Object.keys(schema);
-    const ajv = new AJV({ allErrors: true });
-    ajvErrors(ajv);
-    ajvFormats(ajv);
+
+    requestProperties.forEach((property) => {
+        const requestPropertySchema = schema[property];
+        const validationFunction = ajv.compile(requestPropertySchema);
+
+        compiledValidationFunctions[property] = validationFunction;
+    });
 
     return (request) => {
         requestProperties.forEach((property) => {
-            const propertyData = request[property];
-            const propertySchema = schema[property];
-            if (!ajv.validate(propertySchema, propertyData)) {
-                throw ajv.errors.map(
+            const validationFunction = compiledValidationFunctions[property];
+            if (!validationFunction) return;
+
+            const requestPropertyData = request[property];
+            const isValid = validationFunction(requestPropertyData);
+
+            if (!isValid) {
+                throw validationFunction.errors.map(
                     ({ message }) => new GenericError(400, message)
                 );
             }
